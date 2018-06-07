0d89c2be8a365fae7cf38675eaa4fda2667d3e5b
Alexandru Matei Prodrom <prodromalex@yahoo.com>
2018-06-07 10:33:33 +0200

    initial project with running spring and some libraries

---
+++ src/main/java/api/WebCrawlerController.java
@@ -0,0 +1,14 @@
+package api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/web-crawler")
+public class WebCrawlerController {
+
+    @RequestMapping("/home")
+    public String home() {
+        return "Let's crawl the web guys!";
+    }
+}
