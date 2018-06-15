bffd6a12f1997ac2eb21246d25a065648a29af64
Jack D <dmarkov23@gmail.com>
2018-06-15 22:06:20 +0200

    setup of api architecture (basic classes and api calls pipeline)

--- src/main/java/api/controllers/WebCrawlerController.java
+++ src/main/java/api/controllers/WebCrawlerController.java
@@ -1,4 +1,4 @@
-package api;
+package api.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/crawling/CrawlingManager.java
@@ -0,0 +1,29 @@
+package crawling;
+
+import api.response.models.CrawlingJobData;
+import api.response.models.CrawlingResult;
+
+/**
+ * The only purpose of this class is to work with the Controller and combine and return all crawling
+ * results into a response object that the controller sends as a HTTP response to the requesting client
+ */
+public class CrawlingManager {
+    public CrawlingResult crawlAllProducts() {
+        // Here we call tha actual CrawlingApp class
+
+        return new CrawlingResult("", 1);
+    }
+
+    public CrawlingResult crawlProductsByName(String name) {
+        // Here we call tha actual CrawlingApp class
+
+        return new CrawlingResult("", 1);
+
+    }
+
+    public CrawlingJobData getCrawlingJobDataByID(int crawlingJobID) {
+        // Here we call tha actual CrawlingApp class
+
+        return new CrawlingJobData(1, 1, 1, 1);
+    }
+}
