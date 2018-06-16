9e87e9b35b7c6985fb6d13c2e8567067b8470db2
Alexandru Matei Prodrom <prodromalex@yahoo.com>
2018-06-16 23:33:22 +0200

    shouldAddPagesToTheList implementation added and test passing

--- src/main/java/crawling/trackers/BfsPageTracker.java
+++ src/main/java/crawling/trackers/BfsPageTracker.java
@@ -2,11 +2,22 @@ package crawling.trackers;
 
 import crawling.interfaces.IPageTracker;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BfsPageTracker implements IPageTracker {
+
+    private List<String> pagesToScan;
+    private int nextPage = 0;
+
+    public BfsPageTracker() {
+        pagesToScan = new ArrayList<>();
+    }
+
     @Override
-    public void addPages(List<String> pages) { }
+    public void addPages(List<String> pages) {
+        pagesToScan.addAll(pages);
+    }
 
     @Override
     public int getMaximumScanDepth() {
@@ -25,6 +36,8 @@ public class BfsPageTracker implements IPageTracker {
 
     @Override
     public Boolean hasNext() {
-        return null;
+        if(nextPage+1 <= pagesToScan.size())
+            return true;
+        return false;
     }
 }
