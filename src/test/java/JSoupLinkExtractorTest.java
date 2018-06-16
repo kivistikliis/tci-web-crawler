4a610d99dcb1870b0541ce97d5261e0d1269a238
kivistikliis <liis.kivistik@cytosmart.com>
2018-06-16 14:09:39 +0200

    Added temporary test to fail CircleCI

--- src/test/java/JSoupLinkExtractorTest.java
+++ src/test/java/JSoupLinkExtractorTest.java
@@ -1,2 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class JSoupLinkExtractorTest {
+
+    @Test
+    public void thisTestShouldFailCircleCi(){
+        String toTest = "fail";
+        Assert.assertEquals("success", toTest);
+    }
 }
