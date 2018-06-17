385821fd63f6c8bf20ca909e397c7e3c5139e999
Jack D <dmarkov23@gmail.com>
2018-06-17 20:44:59 +0200

    created some test for CrawlingApp and CrawlingJob

--- src/test/java/CrawlingJobTest.java
+++ src/test/java/CrawlingJobTest.java
@@ -2,6 +2,7 @@ import crawling.interfaces.ILinkExtractor;
 import crawling.interfaces.IPageRetriever;
 import crawling.interfaces.IPageTracker;
 import crawling.interfaces.IProductExtractor;
+import crawling.models.CrawlingResult;
 import crawling.process.CrawlingJob;
 import org.junit.Test;
 
@@ -9,32 +10,39 @@ import java.net.ConnectException;
 import java.net.UnknownHostException;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 public class CrawlingJobTest {
 
     @Test
     public void startShouldReturnValidObjectWhenNoExceptionWasThrown() {
 
-        ILinkExtractor linkExtractor = null;
-        IPageRetriever pageRetriever = null;
-        IPageTracker pageTracker = null;
-        IProductExtractor productExtractor = null;
+        // Arrange
+        ILinkExtractor mockedLinkExtractor = mock(ILinkExtractor.class);
+        IPageRetriever mockedPageRetriever = mock(IPageRetriever.class);
+        IPageTracker mockedPageTracker = mock(IPageTracker.class);
+        IProductExtractor mockedProductExtractor = mock(IProductExtractor.class);
 
-        CrawlingJob crawlingJob = new CrawlingJob(linkExtractor, pageRetriever, pageTracker, productExtractor);
+        CrawlingJob crawlingJob = new CrawlingJob(mockedLinkExtractor, mockedPageRetriever, mockedPageTracker, mockedProductExtractor);
+        // Act
+        CrawlingResult crawlingResult = crawlingJob.start();
 
-
+        // Assert
+        // Do some assertions, checking if the result is valid
+        assertFalse(crawlingResult.getRetrievedData().equals(""));
+        assertTrue(crawlingResult.getCrawlingJobID() > 0);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void startShouldThrowExceptionWhenPageRetrieverUrlIsIncorrect() {
-
+        // TODO When interface implementations are merged
     }
 
     @Test(expected = UnknownHostException.class)
     public void startShouldThrowExceptionWhenPageRetrieverUrlIsUnreachable() {
+        // TODO When interface implementations are merged
 
     }
-
 
 
 }
